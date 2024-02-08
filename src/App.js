fc8d63ebf4087e73c5b8f59f0450035a1d0d8587
rohitdasari0424 <rohitdasari75@gmail.com>
2024-02-08 16:14:58 -0500

    Using states to make interactive Employee app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import './App.css';
+import Employee from './Components/Employee.js';
+import { useState } from 'react';
 
 function App() {
+  const [employees, setEmployees] = useState(
+    [
+      {
+          name: "Rohit Dasari",
+          role: "CEO"
+      },
+      {
+          name: "Bunty Babu",
+          role: "Dev"
+      },
+      {
+          name: "Ishan Patel",
+          role: "Senior Developer"
+      },
+      {
+          name: "Abhiram Menon",
+          role: "Lawyer"
+      }
+    ]
+  );
+  const showEmployees = true;
+
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
-        <p>
+        <p className='text-center mb-10'>
           Hello, welcome to the application!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {showEmployees ? (
+          <>
+            <div className='flex flex-wrap justify-center'>
+                {employees.map((employee) => {
+                  return (
+                    <Employee name={employee.name} role={employee.role}/>
+                  );
+                })}
+            </div>
+          </>
+        ):(
+          <h3> No employees to be shown. </h3>
+        )}
+        
       </header>
     </div>
   );
